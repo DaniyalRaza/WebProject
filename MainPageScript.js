e012b0bea59e037b21473332321d15a8023dc9fb
Daniyal Raza <daniyalraza26@gmail.com>
2015-12-06 17:18:44 +0500

    Local stream added

--- MainPageScript.js
+++ MainPageScript.js
@@ -4,6 +4,7 @@
 
 
 var video_out = document.getElementById("vid-box");
+var vid_thumb = document.getElementById("vid-thumb");
 
 function login(form) {
     var phone = window.phone = PHONE({
@@ -11,10 +12,12 @@ function login(form) {
         publish_key   : 'pub-c-9df37493-406f-4ccd-8200-8ea58cb55aea',
         subscribe_key : 'sub-c-8a9be808-8fb3-11e5-b303-0619f8945a4f'
     });
-    phone.ready(function(){form.username.style.background="#55ff5b"; });
-    phone.receive(function(session){
-        session.connected(function(session) { video_out.appendChild(session.video); showModal();});
-        session.ended(function(session) { video_out.innerHTML=''; });
+    var ctrl = window.ctrl = CONTROLLER(phone);
+
+    ctrl.ready(function(){form.username.style.background="#55ff5b"; });
+    ctrl.receive(function(session){
+        session.connected(function(session) { video_out.appendChild(session.video);ctrl.addLocalStream(vid_thumb); showModal();});
+        session.ended(function(session) { video_out.innerHTML='';vid_thumb.innerHTML='';ctrl.getVideoElement(session.number).remove(); });
     });
     return false;
 }
